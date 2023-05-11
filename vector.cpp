fe22c7d95201fd0764620eaeabf78aecd2d3ed8d
saloni2811 <saloni28singh2001@gmail.com>
2023-05-11 17:49:54 +0530

    Revising

--- vector.cpp
+++ vector.cpp
@@ -1,51 +1,57 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+
 using namespace std;
+
 int main()
 {
-    vector<int> v;
-    vector<int> a(5,1);//every element will start with 1 ; 5 ka matalb hai size 5 hoga
-    cout<<"print a"<<endl;
-    for(int i:a){
-        cout<<i<<" ";
-    }
-    cout<<endl;
-    vector<int> last(a);//a ko copy kar dega
-    cout<<"print last: "<<endl;for(int i:last){
-        cout<<i<<" ";
-    }
-    cout<<endl;
-    cout<<"capacity: "<<v.capacity()<<endl;
-    v.push_back(1);
-     cout<<"capacity: "<<v.capacity()<<endl;
-     v.push_back(2);
-      cout<<"capacity: "<<v.capacity()<<endl;
-      v.push_back(3);//bracket mai size hai 
-      cout<<"capacity: "<<v.capacity()<<endl;//khud ko double kr leta h
-      cout<<"size: "<<v.size()<<endl;
-    cout<<"Element at 2nd index: "<<v.at(2)<<endl;
-      cout<<"first element: "<<v.front()<<endl;
-      cout<<"last element: "<<v.back()<<endl;
-      cout<<"before pop"<<endl;
-      for(int i:v)
-      {
-        cout<<i<<" ";
-      }
-      cout<<endl;
-      v.pop_back();//size decrease krta hai
-      cout<<"after pop: "<<endl;
-      for(int i:v){
-        cout<<i<<" ";
-      }
-      cout<<endl;
-      cout<<"before clear size: "<<v.size()<<endl;
-      v.clear();//clears the size 
-      cout<<"after clear size: "<<v.size()<<endl;
 
+  vector<int> a(5, 1);
+  cout << "print a" << endl;
+  for (int i : a)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
 
+  vector<int> last(a) ;
+   cout << "Print last" << endl;
+  for (int i : last)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
 
+  vector<int> v;
+  cout << "Capacity: " << v.capacity() << endl;
 
-    
+  v.push_back(1);
+  cout << "Capacity: " << v.capacity() << endl;
 
+  v.push_back(3);
+  cout << "Capacity: " << v.capacity() << endl;
 
+  v.push_back(5);
+  cout << "Capacity: " << v.capacity() << endl;
+  cout << "Size: " << v.size() << endl;
+  cout << "Before Pop" << endl;
+  for (int i : v)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  v.pop_back();
+
+  cout << "After Pop" << endl;
+  for (int i : v)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  cout << "at operation: " << v.at(1) << endl;
+
+  cout << "Element at front: " << v.front() << endl;
+  cout << "Element at back: " << v.back() << endl;
 }
