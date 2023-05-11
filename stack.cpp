fcb310ab79d7800e8bd5bb302700b2c52f3ac7a9
saloni2811 <saloni28singh2001@gmail.com>
2023-05-11 18:16:14 +0530

    Revising

--- stack.cpp
+++ stack.cpp
@@ -1,21 +1,18 @@
-#include<iostream>
-#include<stack>
+#include <iostream>
+#include<bits/stdc++.h >
 using namespace std;
-int main(){
-    stack<string> s;
-    s.push("Saloni");
-        s.push("Singh");
+int main()
+{
+    stack<int> s;
+    s.push(14);
+    s.push(04);
 
-    s.push("Love");
-    cout<<"top element: "<<s.top()<<endl;
-    cout<<"size of the stack: "<<s.size()<<endl;
+    s.push(2022);
+
+    cout << "top element: " << s.top() << endl;
+    cout << "size of the stack: " << s.size() << endl;
     s.pop();
-        cout<<"top element: "<<s.top()<<endl;
-            cout<<"size of the stack: "<<s.size()<<endl;
-            cout<<"empty or not: "<<s.empty()<<endl;
-
-
-
-
-
+    cout << "top element: " << s.top() << endl;
+    cout << "size of the stack: " << s.size() << endl;
+    cout << "empty or not: " << s.empty() << endl;
 }
