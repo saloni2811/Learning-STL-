14ef71a8eecf3e78b3e2aaf71cab073dba8bf9e4
saloni2811 <saloni28singh2001@gmail.com>
2023-05-11 17:15:52 +0530

    Revising

--- stl_array.cpp
+++ stl_array.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 #include<array>
+
 using namespace std;
+
 int main(){
-    int bascic[7]={1,2,3,4,5,6,7};//basic array
-    array<int,8>a ={1,2,3,4,5,6,7,8};//stl array
-    int size=a.size();//size of array
-    for(int i=0; i<size;i++){
-        cout<<a[i]<<endl;
+    array<int,5> arr={1,2,3,4,5};
+    int size = arr.size();
+    cout<<"Size of the array: "<<size<<endl;
+    for(int i = 0;i<size;i++){
+        cout<<arr[i]<<endl;
     }
-    cout<<"Element at 7th Index: "<<a.at(7)<<endl;
-    cout<<"Empty or not:  "<<a.empty()<<endl;
-    cout<<"First element: "<<a.front()<<endl;
-    cout<<"Last element: "<<a.back()<<endl;
-      
+    cout<<"Empty or not: "<<arr.empty()<<endl;
+    cout<<"Element ar 3rd index: "<<arr.at(3)<<endl;
+
+    cout<<"Element at front: "<<arr.front()<<endl;
+    cout<<"Element at back: "<<arr.back();
+
+
 }
